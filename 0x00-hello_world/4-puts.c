b0929f57553463c718ff8db0399805cbc8733643
emmanuelmakori <emmanuelmoywaywamakori@gmail.com>
2023-03-16 12:52:47 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>                                                                  
-/**                                                                                 
- *  * Description: main - Prints out a string to stdout.                            
- *   * Return: 0 if success.                                                        
-*/                                                                                  
-int main(void)                                                                      
-{                                                                                   
-printf("\""Programming is like building a multilingual puzzle");                
-return (0);}
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
