4850310cb5fe640ec5424a849b8d81d6ca11892a
emmanuelmakori <emmanuelmoywaywamakori@gmail.com>
2023-03-16 12:45:01 -0700

    frere

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-/**
- * main - A program that prints a line with puts function
- * Return: # (Success)
- */
-int main(void)
-{
-puts("\""Programming is like building a multilingual puzzle");
-return(0);
-}
+#include <stdio.h>                                                                  
+/**                                                                                 
+ *  * Description: main - Prints out a string to stdout.                            
+ *   * Return: 0 if success.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("\""Programming is like building a multilingual puzzle");                
+return (0);}
