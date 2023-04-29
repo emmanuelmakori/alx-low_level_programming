f6c0477b8360397df8555da835ef03f89938600f
emmanuelmakori <makmanuel101@gmail.com>
2023-04-29 11:09:00 -0700

    new 0 question

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "list.h"
-#include <stdio.h>
 
 /**
  * print_listint - prints all listint_t list.
@@ -11,13 +10,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+        size_t number = 0; // Initialize a variable to store the number of nodes
+        struct list_head *pos; // Declare a pointer to a list_head structure
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		number++;
-	}
-	return (number);
+        // Use the list_for_each macro to iterate over the list
+        list_for_each(pos, h) {
+                // Get the parent container of the list entry using the list_entry macro
+                listint_t *node = list_entry(pos, listint_t, head);
+                // Print the integer value stored in the node
+                printf("%d\n", node->n);
+                // Increment the number of nodes
+                number++;
+        }
+        // Return the number of nodes
+        return (number);
+
 }
