41449079f9371f4164ed0cb9251cf5fbdced03e1
emmanuelmakori <makmanuel101@gmail.com>
2023-05-03 13:51:06 -0700

    task10 redone

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,43 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node which correspond to the index of a listint_t list.
- *
- * @head: this is the pointer to the address of head of the listint_t list.
- * 
- * @index: The index of the node not wanted.
- *
- * Return: for success = 1.
- *         for failure = -1.
+ * delete_nodeint_at_index - removes the node at index of the linked list specified
+ * @head: double to the pointer
+ * @index : the index of the node that should be removed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *id = *head;
-	unsigned int node;
+	/*Declaring list and variables*/
+	listint_t *first = *head, *var;
+	unsigned int i = 0;
 
-	if (id == NULL)
+	/*if head is empty*/
+	if (*head == NULL)
 		return (-1);
 
-	if (index == 0)
+	if (index == 0) /*if index is start*/
 	{
-		*head = (*head)->next;
-		free(id);
+		*head = first->next;
+		free(first);
 		return (1);
 	}
 
-	for (node = 0; node < (index - 1); node++)
+	while (first != NULL && i < (index - 1))
 	{
-		if (id->next == NULL)
-			return (-1);
-
-		id = id->next;
+		first = first->next;
+		i++;
 	}
 
-	temp = id->next;
-	id->next = temp->next;
-	free(temp);
-	return (-1);
+	/*Check their current node and the next node*/
+	if (first == NULL || first->next == NULL)
+		return (-1);
+
+	/*Delete the node which corresponds the index*/
+	var = first->next;
+	first->next = var->next;
+
+	free(var);
+
+	return (1);
 }
